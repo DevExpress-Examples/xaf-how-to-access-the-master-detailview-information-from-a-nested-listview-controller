99a28fd3c9a7faaceea4c5b4fa5053e40f63fbb6
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WinWebSolution.Module/PhoneNumberNestedListViewController.vb
@@ -0,0 +1,31 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Reflection
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Actions
+Imports DevExpress.Persistent.BaseImpl
+
+Namespace WinWebSolution.Module
+	Public Class PhoneNumberNestedListViewController
+		Inherits NestedListViewControllerBase
+		Private saMasterDetailViewInfoAction As SimpleAction = Nothing
+		Public Sub New()
+			TargetObjectType = GetType(PhoneNumber)
+			saMasterDetailViewInfoAction = New SimpleAction(Me, "MasterDetailViewInfoAction", DevExpress.Persistent.Base.PredefinedCategory.View)
+			AddHandler saMasterDetailViewInfoAction.Execute, AddressOf saMasterDetailViewInfoAction_Execute
+		End Sub
+		Private Sub saMasterDetailViewInfoAction_Execute(ByVal sender As Object, ByVal e As SimpleActionExecuteEventArgs)
+			MasterDetailViewInfo()
+		End Sub
+		Private Sub MasterDetailViewInfo()
+			Console.WriteLine(String.Format("MasterViewId = '{0}'", Me.MasterDetailViewId))
+		End Sub
+		Protected Overrides Sub OnActivated()
+			MyBase.OnActivated()
+			AddHandler View.ControlsCreated, AddressOf View_ControlsCreated
+		End Sub
+		Private Sub View_ControlsCreated(ByVal sender As Object, ByVal e As EventArgs)
+			MasterDetailViewInfo()
+		End Sub
+	End Class
+End Namespace
---
+++ VB/WinWebSolution.Win/WinApplication.Designer.vb
@@ -0,0 +1,64 @@
+Imports Microsoft.VisualBasic
+Imports System
+Namespace WinWebSolution.Win
+	Partial Public Class WinWebSolutionWindowsFormsApplication
+		''' <summary> 
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary> 
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Component Designer generated code"
+
+		''' <summary> 
+		''' Required method for Designer support - do not modify 
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.module1 = New DevExpress.ExpressApp.SystemModule.SystemModule()
+			Me.module2 = New DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule()
+			Me.module3 = New WinWebSolution.Module.WinWebSolutionModule()
+			Me.module6 = New DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule()
+			Me.sqlConnection1 = New System.Data.SqlClient.SqlConnection()
+			Me.fileAttachmentsWindowsFormsModule1 = New DevExpress.ExpressApp.FileAttachments.Win.FileAttachmentsWindowsFormsModule()
+			CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+			' 
+			' sqlConnection1
+			' 
+			Me.sqlConnection1.ConnectionString = "Data Source=(local);Initial Catalog=WinWebSolution;Integrated Security=SSPI;Pooli" & "ng=false"
+			Me.sqlConnection1.FireInfoMessageEventOnUserErrors = False
+			' 
+			' WinWebSolutionWindowsFormsApplication
+			' 
+			Me.ApplicationName = "WinWebSolution"
+			Me.Connection = Me.sqlConnection1
+			Me.Modules.Add(Me.module1)
+			Me.Modules.Add(Me.module2)
+			Me.Modules.Add(Me.module6)
+			Me.Modules.Add(Me.module3)
+			Me.Modules.Add(Me.fileAttachmentsWindowsFormsModule1)
+'			Me.DatabaseVersionMismatch += New System.EventHandler(Of DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs)(Me.WinWebSolutionWindowsFormsApplication_DatabaseVersionMismatch);
+			CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+		End Sub
+
+		#End Region
+
+		Private module1 As DevExpress.ExpressApp.SystemModule.SystemModule
+		Private module2 As DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule
+		Private module3 As WinWebSolution.Module.WinWebSolutionModule
+		Private module6 As DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule
+		Private sqlConnection1 As System.Data.SqlClient.SqlConnection
+		Private fileAttachmentsWindowsFormsModule1 As DevExpress.ExpressApp.FileAttachments.Win.FileAttachmentsWindowsFormsModule
+	End Class
+End Namespace
