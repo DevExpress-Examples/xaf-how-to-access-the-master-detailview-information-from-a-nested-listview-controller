690543c948afa267fdf8076d6f748ed9d39d720a
andrey <andrey.kozhevnikov@devexpress.com>
2023-04-17 17:27:12 +0400

    xpo

---
+++ CS/XPO/GetMasterFrame/GetMasterFrame.Module/Controllers/MasterDetailViewController.cs
@@ -0,0 +1,40 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Editors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMasterFrame.Module.Controllers {
+
+
+    public class MasterDetailViewController : ViewController<DetailView> {
+        private void PushFrameToNestedController(Frame frame) {
+            foreach (Controller c in frame.Controllers) {
+                if (c is NestedListViewFrameController) {
+                    ((NestedListViewFrameController)c).AssignMasterFrame(Frame);
+                }
+            }
+        }
+        private void lpe_FrameChanged(object sender, EventArgs e) {
+            PushFrameToNestedController(((ListPropertyEditor)sender).Frame);
+        }
+        protected override void OnActivated() {
+            base.OnActivated();
+            foreach (ListPropertyEditor lpe in View.GetItems<ListPropertyEditor>()) {
+                if (lpe.Frame != null) {
+                    PushFrameToNestedController(lpe.Frame);
+                } else {
+                    lpe.FrameChanged += lpe_FrameChanged;
+                }
+            }
+        }
+        protected override void OnDeactivated() {
+            foreach (ListPropertyEditor lpe in View.GetItems<ListPropertyEditor>()) {
+                lpe.FrameChanged -= new EventHandler<EventArgs>(lpe_FrameChanged);
+            }
+            base.OnDeactivated();
+        }
+    }
+}
